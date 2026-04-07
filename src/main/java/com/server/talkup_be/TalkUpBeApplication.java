28648bba786883bf2a013a1c9e7a610dc6247930
노현경 <pause13818@gmail.com>
2026-04-08 03:57:43 +0900

    BE_init

---
+++ src/main/java/com/server/talkup_be/TalkUpBeApplication.java
@@ -0,0 +1,13 @@
+package com.server.talkup_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkUpBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalkUpBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/server/talkup_be/TalkUpBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.talkup_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkUpBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
